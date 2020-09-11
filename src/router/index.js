568136151d236b73b1a7344ecdf87154e9810919
Marina Grechukha <marina.grechukha@gmail.com>
2020-09-11 14:42:20 +0300

    add vues and routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,62 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
+    path: '/login',
+    name: 'Login',
+    meta: { layout: 'empty-layout' },
+    component: () => import('../views/Login.vue'),
+  },
+  {
+    path: '/registration',
+    name: 'Registration',
+    meta: { layout: 'empty-layout' },
+    component: () => import('../views/Registration.vue'),
+  },
+  {
     path: '/',
     name: 'Home',
-    component: Home,
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Home.vue'),
+  },
+  {
+    path: '/categories',
+    name: 'Categories',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Categories.vue'),
+  },
+  {
+    path: '/detail/:id',
+    name: 'Detail',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Detail.vue'),
+  },
+  {
+    path: '/history',
+    name: 'History',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/History.vue'),
+  },
+  {
+    path: '/plan',
+    name: 'Plan',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Plan.vue'),
+  },
+  {
+    path: '/profile',
+    name: 'Profile',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Profile.vue'),
+  },
+  {
+    path: '/record',
+    name: 'Record',
+    meta: { layout: 'main-layout' },
+    component: () => import('../views/Record.vue'),
   },
 ]
 
