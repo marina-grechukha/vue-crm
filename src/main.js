ed5fc9488cb1f8be7cc7834b0c6d8df533d5cd26
Marina Grechukha <marina.grechukha@gmail.com>
2020-09-16 12:16:42 +0300

    add message plugin for logout

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Vuelidate from 'vuelidate'
 import dateFilter from '@/filters/date.filter'
+import messagePlugin from '@/utils/message.plugin'
+import M from 'materialize-css'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -9,8 +11,9 @@ import 'materialize-css/dist/js/materialize.min'
 
 Vue.config.productionTip = false
 
+Vue.use(M)
+Vue.use(messagePlugin)
 Vue.use(Vuelidate)
-
 Vue.filter('date', dateFilter)
 
 new Vue({
