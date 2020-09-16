ea7d6a76014723b44fceb175327bfce71acc48cc
Marina Grechukha <marina.grechukha@gmail.com>
2020-09-16 15:47:51 +0300

    add firebase

--- src/main.js
+++ src/main.js
@@ -3,6 +3,9 @@ import Vuelidate from 'vuelidate'
 import dateFilter from '@/filters/date.filter'
 import messagePlugin from '@/utils/message.plugin'
 import M from 'materialize-css'
+import firebase from 'firebase/app'
+import 'firebase/auth'
+import 'firebase/database'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
@@ -16,8 +19,24 @@ Vue.use(messagePlugin)
 Vue.use(Vuelidate)
 Vue.filter('date', dateFilter)
 
-new Vue({
-  router,
-  store,
-  render: (h) => h(App),
-}).$mount('#app')
+firebase.initializeApp({
+  apiKey: 'AIzaSyA48aJ1TGr5QvqcLkgFsCU_vyJk4dfiKWQ',
+  authDomain: 'vue-crm-mg.firebaseapp.com',
+  databaseURL: 'https://vue-crm-mg.firebaseio.com',
+  projectId: 'vue-crm-mg',
+  storageBucket: 'vue-crm-mg.appspot.com',
+  messagingSenderId: '754791876484',
+  appId: '1:754791876484:web:da11406996cb50d1b93fd1'
+})
+
+let app
+
+firebase.auth().onAuthStateChanged(() => {
+  if (!app) {
+    app = new Vue({
+      router,
+      store,
+      render: (h) => h(App),
+    }).$mount('#app')
+  }
+})
